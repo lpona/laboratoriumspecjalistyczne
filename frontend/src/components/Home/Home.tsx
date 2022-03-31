e50220d88bddb1fe0c677b7b799b0684554f9990
l.ponachajba@medidok.de <l.ponachajba@medidok.de>
2022-03-31 17:27:47 +0200

    Hometsx/scss

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Footer from '../Footer/Footer';
-
+import "./Home.scss";
 function Home() {
   return (
     <div>
-      Welcome to the Home Page!
+      <div className='background'>
+      <div className="ipc"><div>The premier source of global entertainment metadata and box office revenue</div></div>
+<h1>Everything Entertainment</h1>
+<div className='coto'></div>
+
+      </div>
       <Footer />
     </div>
   );
