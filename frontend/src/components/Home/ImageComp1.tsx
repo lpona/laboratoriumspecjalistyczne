2e41bc07d7bfcf55f9fe8db81307b870a673b921
barteko <bartek.olek123@wp.pl>
2022-05-11 10:23:31 +0200

    Add slider to Home

---
+++ frontend/src/components/Home/ImageComp1.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+
+function ImgComp1() {
+
+  let imgStyles = {
+    width: 100 + "%",
+    height: "auto"
+
+  }
+  return (
+    <div className="background">
+      <h1>Everything Entertainment</h1>
+      <div className="info">
+        <div className="items">
+          The premiere source of global
+          <br /> entertainment metadata and box
+          <br />
+          office revenue
+        </div>
+        <div className="items">
+          8+ milion titles
+          <br /> 11+ cast and crew
+          <br /> Global Box Office data
+        </div>
+        <div className="items">
+          1 billion ratings from the word's
+          <br /> largest entertainment fan
+          <br /> community
+        </div>
+      </div>
+    </div>
+
+  )
+
+}
+
+export default ImgComp1;
