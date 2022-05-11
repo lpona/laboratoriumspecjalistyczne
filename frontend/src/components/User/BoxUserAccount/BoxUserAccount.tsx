0b6589d5cfe3e1a72b4996b38f79474b32e06b25
Damian Łaganowski <damian7507@gmai.com>
2022-05-11 11:43:39 +0200

    Little fixes & add user box icon

--- frontend/src/components/User/BoxUserAccount/BoxUserAccount.tsx
+++ frontend/src/components/User/BoxUserAccount/BoxUserAccount.tsx
@@ -1,17 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { NavLink, useRouteMatch, Route, Switch } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { NavLink, useRouteMatch, Route, Switch } from "react-router-dom";
 
-import { RootState } from '../../../rtk/store';
-import useSignOut from '../../../hooks/useSignOut';
+import { RootState } from "../../../rtk/store";
+import useSignOut from "../../../hooks/useSignOut";
 
-import BoxNameChange from '../BoxNameChange/BoxNameChange';
-import BoxPasswordChange from '../BoxPasswordChange/BoxPasswordChange';
-import BoxEmailChange from '../BoxEmailChange/BoxEmailChange';
-import BoxAccountData from '../BoxAccountData/BoxAccountData';
-import Footer from '../../Footer/Footer';
+import BoxNameChange from "../BoxNameChange/BoxNameChange";
+import BoxPasswordChange from "../BoxPasswordChange/BoxPasswordChange";
+import BoxEmailChange from "../BoxEmailChange/BoxEmailChange";
+import BoxAccountData from "../BoxAccountData/BoxAccountData";
+import Footer from "../../Footer/Footer";
 
-import './BoxUserAccount.scss';
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import DriveFileRenameOutlineIcon from "@mui/icons-material/DriveFileRenameOutline";
+import PasswordIcon from "@mui/icons-material/Password";
+import MailIcon from "@mui/icons-material/Mail";
+import LogoutIcon from "@mui/icons-material/Logout";
+
+import "./BoxUserAccount.scss";
 
 const BoxUserAccount = (props: any) => {
   const { firstName } = useSelector((state: RootState) => state.currentUser);
@@ -35,45 +41,49 @@ const BoxUserAccount = (props: any) => {
       <div className="box-user-account">
         <div className="box-user-account-container">
           {!visible && (
-            <div className='box-user-account-navigation'>
+            <div className="box-user-account-navigation">
               <NavLink to={url}>back to the user panel</NavLink>
             </div>
           )}
           {visible && (
-            <div className='box-user-account-content'>
+            <div className="box-user-account-content">
               <NavLink to={`${url}/account-data`}>
-                <div className='box-user-account-content-item'>
+                <div className="box-user-account-content-item">
                   <>
+                    <AccountCircleIcon />
                     <p>your data</p>
                   </>
                 </div>
               </NavLink>
               <NavLink to={`${url}/name-change`}>
-                <div className='box-user-account-content-item'>
+                <div className="box-user-account-content-item">
                   <>
+                    <DriveFileRenameOutlineIcon />
                     <p>change name or surname</p>
                   </>
                 </div>
               </NavLink>
               <NavLink to={`${url}/password-change`}>
-                <div className='box-user-account-content-item'>
+                <div className="box-user-account-content-item">
                   <>
+                    <PasswordIcon />
                     <p>change password</p>
                   </>
                 </div>
               </NavLink>
               <NavLink to={`${url}/email-change`}>
-                <div className='box-user-account-content-item'>
+                <div className="box-user-account-content-item">
                   <>
+                    <MailIcon />
                     <p>change email</p>
                   </>
                 </div>
               </NavLink>
               <div
-                className='box-user-account-content-item logout'
-                onClick={() => signOutOnClickHandler()}
-              >
+                className="box-user-account-content-item logout"
+                onClick={() => signOutOnClickHandler()}>
                 <>
+                  <LogoutIcon />
                   <p>logout</p>
                 </>
               </div>
